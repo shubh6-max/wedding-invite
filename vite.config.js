112afe4e6d2e1fcaafdf8f190695ac1ece4e6efa
shubh6-max <shubh6-max@users.noreply.github.com>
2026-05-07 23:51:42 +0530

    Support both Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/wedding-invite/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github-pages' ? '/wedding-invite/' : '/',
   plugins: [react()],
-})
+}))
